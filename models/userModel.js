5e0d4ed72f2d57b48c54609f422bf0fb14b16414
Dev <dev@Devs-MacBook-Air.local>
2024-03-21 00:55:50 +0530

    pass: set not require

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     userName: {type: String},
     email: {type: String, require: true},
-    password: {type: String, require: true},
+    password: {type: String},
     lastOtp: {type: String},
     deviceId: {type: String},
     employeeId: {type: String},
